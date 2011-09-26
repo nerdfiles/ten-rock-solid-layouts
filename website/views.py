9dae13a45ab381085fbd7127bf10084b80fedf12
Aaron Alexander <nerdfiles@gmail.com>
2011-09-25 20:19:32 -0500

    cleanup

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response, get_object_or_404, get_list_or_
 
 # ============================================================ VIEWS FUNCTIONS
 
-def index(request):
+def index(request, context):
   #latest_OBJ_list = OBJ.objects.all().order_by('-pub_date')[:5]
   #return render_to_response('website/index.html', {'latest_OBJ_list': latest_OBJ_list})
-  return render_to_response('website/base.html', context_instance=RequestContext(request))
+  return render_to_response('website/base.html', context_instance=RequestContext(request, context))
